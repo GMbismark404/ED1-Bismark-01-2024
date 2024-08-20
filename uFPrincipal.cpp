3abd694e4f18b57cea2de27aef2add0935361632
GMBismark404 <bismarkaguilar59@gmail.com>
2024-08-20 04:00:46 -0400

    commit 1

---
+++ uFPrincipal.cpp
@@ -0,0 +1,56 @@
+//---------------------------------------------------------------------------
+#include <vcl.h>
+#pragma hdrstop
+#include "uFPrincipal.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm1 *Form1;
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::CrearMemoriaClick(TObject *Sender)
+{
+	mem = new TCSMemoria();
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::PedirEspacioClick(TObject *Sender)
+{
+	AnsiString text = Edit1->Text;
+	espacio_actual = mem->new_espacio(std::string(text.c_str()));
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::LiberarEspacioClick(TObject *Sender)
+{
+	mem->delete_espacio(StrToInt(Edit2->Text));
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::MostrarMemoriaClick(TObject *Sender)
+{
+	LimpiarCanvas(Form1->Canvas); // Limpia el canvas antes de dibujar
+	mem->Mostrar(Form1->Canvas, StrToInt(Edit3->Text), StrToInt(Edit4->Text));
+}
+
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::PonerDatoClick(TObject *Sender)
+{
+	AnsiString text = Edit6->Text;
+	mem->poner_dato(StrToInt(Edit5->Text), std::string(text.c_str()), StrToInt(Edit7->Text));
+}
+//---------------------------------------------------------------------------
+
+void TForm1::LimpiarCanvas(TCanvas *canvas)
+{
+	// Define el área que deseas limpiar
+    TRect rect(0, 0, ClientWidth, ClientHeight); // Limpia todo el formulario
+    canvas->Brush->Color = clWhite; // Color de fondo (blanco)
+	canvas->FillRect(rect); // Rellena el área con el color de fondo
+}
+
---
+++ uTCSMemoria.h
@@ -0,0 +1,83 @@
+//---------------------------------------------------------------------------
+#ifndef uTCSMemoriaH
+#define uTCSMemoriaH
+#include <string>
+#include <iostream>
+#include <Vcl.Graphics.hpp>
+//---------------------------------------------------------------------------
+using namespace std;
+
+const int MAX = 20;
+const int NULO = -1;
+
+typedef int direccion;
+
+struct NodoM {
+	int dato;
+	string id;
+	direccion link;
+};
+
+class TCSMemoria {
+	private:
+		NodoM *mem;
+		direccion libre;
+	public:
+		TCSMemoria();
+		direccion new_espacio(string cadena);
+		void delete_espacio(direccion dir);
+		void poner_dato(direccion dir, string cadena_id, int valor);
+		int obtener_dato(direccion dir, string cadena_id);
+		int espacio_disponible();
+		int espacio_ocupado();
+		bool dir_libre(direccion dir);
+		int numero_ids(string cadena);
+		string eliminar_flecha(string cadena_id);
+		string obtener_id(string cadena, int pos);
+		void Mostrar(TCanvas *canvas, int desde, int hasta);
+};
+
+int TCSMemoria::numero_ids(string id) { // "dia,mes,año"
+int c = 0;
+	if (id == ""){
+		return c;
+	}else{
+		for (int i = 0; i < id.length() - 1; i++)
+			if ((id[i] != ',') && (id[i+1] == ','))
+				c++;
+
+		if (id[id.length() - 1] != ',')
+			c++;
+	}
+return c;
+}
+
+string TCSMemoria::obtener_id(string cad, int pos) {  // "ca,na,ma"
+int p = 0; string s = ""; int c = 0;
+
+	while (cad[c] == ',')
+		c++;
+
+	for (int i = c; i < cad.length(); i++) {
+		if (cad[i] == ',') {
+			i++; p++;
+		}
+
+		if (p == pos) {
+			s = s + cad[i];
+		}
+	}
+return s;
+}
+
+string TCSMemoria::eliminar_flecha(string cadena_id) {
+	string cad;
+    for (char c : cadena_id) {
+        if (c != '-' && c != '>') {
+			cad += c;
+        }
+    }
+	return cad;
+}
+
+#endif
